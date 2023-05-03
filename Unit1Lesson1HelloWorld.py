1d64d37cb090ef0e0f8970316f018dfc9531c475
jusbro <97056196+jusbro@users.noreply.github.com>
2023-05-03 14:00:51 -0400

    Update Unit1Lesson1HelloWorld.py

--- Unit1Lesson1HelloWorld.py
+++ Unit1Lesson1HelloWorld.py
@@ -1,3 +1,34 @@
 #This is your first program in Python!
 #It is traditional to start with a "Hello World!" program.
-#Type the line below into this editor and Run the program.
+
+#To print a message to the screen, use the print() function.
+#The print() function takes a string as an argument.
+#A string is a sequence of characters.
+#Strings are enclosed in single or double quotes.
+
+#Example:
+#print("Hello World!")
+
+#Do you notice the "#" symbol at the beginning of some lines?
+#This is called a comment.
+#Comments are ignored by the Python interpreter.
+#They are used to explain what the code is doing.
+#Comments are very important for making code readable.
+#You should comment your code as much as possible.
+
+#Try it yourself!
+#Have the computer print "Hello World!" to the screen.
+#Make sure to use the print() function.
+#Make sure to use quotes around the string.
+
+
+#DO IT ON THE NEXT LINE:
+
+
+#Run your code using the play button on the top right of the screen
+#You should see "Hello World!" printed to the screen.
+#If you don't see it, ask for help.
+
+#Use the rest of this space to complete the assignment as outlined in the packet.
+#Make sure to comment your code as much as possible.
+
