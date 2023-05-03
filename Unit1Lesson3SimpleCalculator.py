09e3bf1b7e90e672d74108abb9c403a3b4e78914
jusbro <97056196+jusbro@users.noreply.github.com>
2023-05-03 14:02:22 -0400

    Create Unit1Lesson3SimpleCalculator.py

---
+++ Unit1Lesson3SimpleCalculator.py
@@ -0,0 +1,24 @@
+#In this lesson you are building a simple calculator.
+
+#The program should ask the user for two numbers.
+#It should store both numbers as separate variables:
+#The first number should be stored in a variable called "firstNumber".
+#The second number should be stored in a variable called "secondNumber".
+
+#ADD: It should then add the two numbers together and store the result in a variable called "sum".
+#SUBTRACT: It should then subtract the second number from the first number and store the result in a variable called "difference".
+#MULTIPLY: It should then multiply the two numbers together and store the result in a variable called "product".
+#DIVIDE: It should then divide the first number by the second number and store the result in a variable called "quotient".
+
+#Your calculator should then print all of these variables to the screen.
+#Each variable should be printed on a separate line with a label.
+#The output should look something like this:
+
+#Sum: 7
+#Difference: 1
+#Product: 12
+#Quotient: 3.0
+
+#Your calculator will not ask the user what math operation they want to perform, instead it does +,-,*,/ every time
+
+#DO YOUR CODE BELOW THIS LINE:
