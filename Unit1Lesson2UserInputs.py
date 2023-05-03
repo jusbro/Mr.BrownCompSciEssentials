a37465d48de00231f0c7cabd51aebb6532c954a2
jusbro <97056196+jusbro@users.noreply.github.com>
2023-05-03 14:01:37 -0400

    Create Unit1Lesson2UserInputs.py

---
+++ Unit1Lesson2UserInputs.py
@@ -0,0 +1,14 @@
+#In this code, you will practice using the input() function.
+#The input() function is a great way to get user input to use in your program.
+
+#Example:
+#name = input("What is your name? ")
+#This example asks the user for their name and stores it in the variable "name".
+#The program pauses until the user presses the enter key.
+
+#Try it yourself!
+#Ask the user for their favorite color and store it in a variable called "color".
+#Then, print the variable to the screen.
+#Make sure to use the input() function and the print() function.
+
+#DO YOUR CODE BELOW THIS LINE:
